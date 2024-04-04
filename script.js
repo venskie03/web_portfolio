608e7238fbc70dd479684cb47e7512a5101bbae0
venskie03 <kevinkyleganados03@gmail.com>
2024-04-04 22:20:16 +0800

    adding close menu to project

--- script.js
+++ script.js
@@ -1,3 +1,124 @@
+    function gotoupwork() {
+            window.location.href = 'https://www.upwork.com/freelancers/~01528b2b871299a364';
+        }
+
+        const navbar = document.querySelector('header');
+
+        const sectionn = document.querySelectorAll('section');
+        const navlinkss = document.querySelectorAll('nav ul li a');
+
+        window.onscroll = () => {
+            if (window.scrollY > 100) {
+                navbar.classList.add('bg-color-primary-dark');
+                navbar.classList.add('border-b');
+                navbar.classList.add('border-color-gray');
+            } else {
+                navbar.classList.remove('bg-color-primary-dark');
+                navbar.classList.remove('border-b');
+                navbar.classList.remove('border-color-gray');
+            }
+
+            // active link
+            let current = 'home';
+
+            sectionn.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                if (pageYOffset >= sectionTop - 60) {
+                    current = section.getAttribute('id');
+                }
+            })
+            navlinkss.forEach((item) => {
+                item.classList.remove('text-[#3bd6c6]');
+                if (item.href.includes(current)) {
+                    item.classList.add("text-[#3bd6c6]");
+                }
+            });
+        }
+
+        // Mobile menu
+        const hamburger = document.querySelector('#hamburger');
+        const menu = document.querySelector('#menu');
+        const hLink = document.querySelectorAll('#hLink');
+        const faSolid = document.querySelector(".fa-solid");
+        const closeicon = document.querySelector('#closeicon');
+
+        hamburger.addEventListener("click", () => {
+            
+            hamburger.classList.add('hidden')
+            closeicon.classList.remove('hidden')
+            closeicon.classList.add('visible')
+        
+            menu.classList.toggle('hidden');
+            faSolid.classList.toggle('fa-xmark');
+      
+          
+        })
+
+        closeicon.addEventListener("click", () => {
+            
+            closeicon.classList.add('hidden')
+            hamburger.classList.remove('hidden')
+            hamburger.classList.add('visible')
+        
+            menu.classList.toggle('hidden');
+            faSolid.classList.toggle('fa-xmark');
+      
+          
+        })
+
+        hLink.forEach(link => {
+            link.addEventListener('click', () => {
+                closeicon.classList.add('hidden');
+                hamburger.classList.remove('hidden')
+                hamburger.classList.add('visible');
+                menu.classList.toggle('hidden');
+                faSolid.classList.toggle('fa-xmark');
+            
+            })
+        })
+
+        // testimonial
+        const userTexts = document.getElementsByClassName('user-text');
+        const userPics = document.getElementsByClassName('user-pic');
+
+        function showReview() {
+            for (userPic of userPics) {
+                userPic.classList.remove("active-pic");
+            }
+            for (userText of userTexts) {
+                userText.classList.remove("active-text");
+            }
+
+            let i = Array.from(userPics).indexOf(event.target);
+
+            userPics[i].classList.add('active-pic');
+            userTexts[i].classList.add('active-text');
+        }
+
+        const toggleBtn = document.getElementById('toggleBtn');
+
+        const card_1_front = document.querySelector("#card_1_front");
+        const card_1_back = document.querySelector("#card_1_back");
+
+        const card_2_front = document.querySelector("#card_2_front");
+        const card_2_back = document.querySelector("#card_2_back");
+
+        const card_3_front = document.querySelector("#card_3_front");
+        const card_3_back = document.querySelector("#card_3_back");
+
+        toggleBtn.addEventListener('change', () => {
+            card_1_front.classList.toggle('-rotate-y-180');
+            card_1_back.classList.toggle('rotate-y-180');
+
+            card_2_front.classList.toggle('-rotate-y-180');
+            card_2_back.classList.toggle('rotate-y-180');
+
+            card_3_front.classList.toggle('-rotate-y-180');
+            card_3_back.classList.toggle('rotate-y-180');
+
+        })
+
+
 function sendEmail(event) {
 
     const name = document.getElementById('name');
